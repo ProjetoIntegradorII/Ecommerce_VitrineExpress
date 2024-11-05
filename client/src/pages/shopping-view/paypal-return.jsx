fc9a2e2d6bc157806857d9da3ff76427ac1401c1
Projeto Integrador II <projetointegradorII014@gmail.com>
2024-11-05 17:51:02 -0300

    Versão 2.0

--- client/src/pages/shopping-view/paypal-return.jsx
+++ client/src/pages/shopping-view/paypal-return.jsx
@@ -1,32 +1,32 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card"; // Importa componentes de UI
+import { capturePayment } from "@/store/shop/order-slice"; // Importa a ação de captura de pagamento
 import { useEffect } from "react"; // Importa o hook useEffect
+import { useDispatch } from "react-redux"; // Importa o hook useDispatch
 import { useLocation } from "react-router-dom"; // Importa o hook useLocation
 
 function PaypalReturnPage() {
+  const dispatch = useDispatch(); // Inicializa o hook useDispatch
   const location = useLocation(); // Captura a localização atual
   const params = new URLSearchParams(location.search); // Cria um objeto URLSearchParams para analisar a string de consulta
   const paymentId = params.get("paymentId"); // Obtém o paymentId dos parâmetros da URL
   const payerId = params.get("PayerID"); // Obtém o payerId dos parâmetros da URL
 
   useEffect(() => {
-    // Efeito para capturar o pagamento quando paymentId e payerId estão presentes
     if (paymentId && payerId) {
       const orderId = JSON.parse(sessionStorage.getItem("currentOrderId")); // Obtém o ID do pedido do sessionStorage
 
-      // Realiza uma requisição GET para capturar o pagamento usando a rota `/shop/paypal-return`
-      fetch(`/shop/paypal-return?paymentId=${paymentId}&PayerID=${payerId}&orderId=${orderId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.success) {
-            sessionStorage.removeItem("currentOrderId"); // Remove o ID do pedido do sessionStorage
+      dispatch(capturePayment({ paymentId, payerId, orderId })).then((data) => {
+        if (data?.payload?.success) {
+          sessionStorage.removeItem("currentOrderId"); // Remove o ID do pedido do sessionStorage
+
+          // Aplica um delay de 3 segundos antes de redirecionar
+          setTimeout(() => {
             window.location.href = "/shop/payment-success"; // Redireciona para a página de sucesso
-          }
-        })
-        .catch((error) => {
-          console.error("Erro ao capturar o pagamento:", error);
-        });
+          }, 3000); // 3000 milissegundos = 3 segundos
+        }
+      });
     }
-  }, [paymentId, payerId]); // Dependências do efeito
+  }, [paymentId, payerId, dispatch]); // Dependências do efeito
 
   return (
     <Card>
